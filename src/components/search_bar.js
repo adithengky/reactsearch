5eefa7dbcedfbd38691ba2254ea6d3ac516a0a6e
Aditya Hengky <aditya@skyshi.com>
2017-07-13 13:13:44 +0700

    modify components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,17 +4,22 @@ class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {term: ''};
+		this.state = {term: 'isyana sarasvati'};
 	}
 	
 	render() {
 		return (
-			<div>
+			<div className="search-bar">
 				<input
 					value =  {this.state.term}
-					onChange={event => this.setState({ term: event.target.value})} />
+					onChange={event => this.onInputChange(event.target.value)} />
 			</div>
 		);
+	}
+
+	onInputChange(term) {
+		this.setState({term});
+		this.props.onSearchTermChange(term);
 	}
 }
 
